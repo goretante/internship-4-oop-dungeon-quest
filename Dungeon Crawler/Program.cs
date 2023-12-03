028c3b36af2fb3abd34df9a9177e66f3e84af4fd
goretante <gospodingoreta@gmail.com>
2023-12-03 22:50:57 +0100

    initial commit

---
+++ Dungeon Crawler/Program.cs
@@ -0,0 +1,33 @@
+﻿
+public class MainMenu
+{
+    public void Start()
+    {
+
+        // main menu
+        Console.WriteLine("~~~ DUNGEON CRAWLER ~~~");
+        Console.WriteLine("1. Započni igru");
+        Console.WriteLine("2. Promijeni postavke");
+        Console.WriteLine("3. Izlaz");
+
+        // user's choice
+        int choice = int.Parse(Console.ReadLine());
+
+        // switch case for user's choice
+        switch (choice)
+        {
+            case 1:
+                // game start
+                // StartGame(); - in progress
+                break;
+            case 2:
+                // change settings
+                // ChangeSettings(); - in progress
+                break;
+            case 3:
+                // exit game
+                // ExitGame(); - in progress
+                break;
+        }
+    }
+}
